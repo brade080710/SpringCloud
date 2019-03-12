34645994e7dbf121973aae4f52436c44c1e9ce98
brade <tianzhihen080710@163.com>
2019-03-12 15:46:29 +0800

    feign propertis配置方式

---
+++ microservice-consumer-movie-feign-config-properies/src/main/java/com/it/cloud/feign/UserFeignClient.java
@@ -0,0 +1,16 @@
+package com.it.cloud.feign;
+
+import com.it.cloud.entity.User;
+import feign.Logger;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "microservice-provider-user")
+public interface UserFeignClient {
+    @GetMapping("/users/{id}")
+    User findById(@PathVariable("id") int id);
+}
+

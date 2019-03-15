e9cf74a14a39d61475e7ea01e2ac516b87d6fc44
brade <tianzhihen080710@163.com>
2019-03-15 17:47:36 +0800

    监控面板，监控微服务情况

--- microservice-consumer-movie-feign-hystrix-dashboard/src/main/java/com/it/cloud/web/HystrixIndexController.java
+++ microservice-consumer-movie-feign-hystrix-dashboard/src/main/java/com/it/cloud/web/HystrixIndexController.java
@@ -3,7 +3,7 @@ package com.it.cloud.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+// 访问进入 http://localhost:8030，将http://localhost:8020/actuator/hystrix.stream输入，就可以监控这个地址服务的情况
 @Controller
 public class HystrixIndexController {
     @GetMapping("")

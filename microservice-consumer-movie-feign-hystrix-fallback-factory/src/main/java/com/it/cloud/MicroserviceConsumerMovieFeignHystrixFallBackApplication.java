32226f70147622012de4b9018bae47b5cc853c13
brade <tianzhihen080710@163.com>
2019-03-15 15:15:37 +0800

    Hystrix fallback factory

---
+++ microservice-consumer-movie-feign-hystrix-fallback-factory/src/main/java/com/it/cloud/MicroserviceConsumerMovieFeignHystrixFallBackApplication.java
@@ -0,0 +1,20 @@
+package com.it.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * feign 默认集成了hystrix，只需要在配置文件中开启就行了
+ */
+@EnableFeignClients
+@SpringBootApplication
+
+public class MicroserviceConsumerMovieFeignHystrixFallBackApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieFeignHystrixFallBackApplication.class, args);
+    }
+
+}

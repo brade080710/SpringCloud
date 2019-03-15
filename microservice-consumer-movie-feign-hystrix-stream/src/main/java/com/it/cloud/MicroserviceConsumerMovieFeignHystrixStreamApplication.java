a3abad8a74c33036394970ff1c3cbb37a388c819
brade <tianzhihen080710@163.com>
2019-03-15 17:15:41 +0800

    feign   集成Hystrix监控

---
+++ microservice-consumer-movie-feign-hystrix-stream/src/main/java/com/it/cloud/MicroserviceConsumerMovieFeignHystrixStreamApplication.java
@@ -0,0 +1,21 @@
+package com.it.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * feign 默认集成了hystrix，只需要在配置文件中开启就行了
+ */
+@EnableFeignClients
+@SpringBootApplication
+@EnableCircuitBreaker
+public class MicroserviceConsumerMovieFeignHystrixStreamApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieFeignHystrixStreamApplication.class, args);
+    }
+
+}

1508b5d7d2a613cd3dea7099b8df9dd3e7628dda
brade <tianzhihen080710@163.com>
2019-03-18 18:09:57 +0800

    Hystrix stream 测试 Turbine

---
+++ microservice-consumer-movie-feign-hystrix-stream-second/src/main/java/com/it/cloud/MicroserviceConsumerMovieFeignHystrixStreamSecondApplication.java
@@ -0,0 +1,22 @@
+package com.it.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * feign 默认集成了hystrix，只需要在配置文件中开启就行了
+ */
+@EnableFeignClients
+@EnableCircuitBreaker
+@SpringBootApplication
+
+public class MicroserviceConsumerMovieFeignHystrixStreamSecondApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieFeignHystrixStreamSecondApplication.class, args);
+    }
+
+}

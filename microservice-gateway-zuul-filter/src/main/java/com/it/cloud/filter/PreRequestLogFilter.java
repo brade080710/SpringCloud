1cbca7d850b4f7495768c079b5a87ce4dc2356f4
brade <tianzhihen080710@163.com>
2019-03-22 10:49:24 +0800

    zuul filter

--- microservice-gateway-zuul-filter/src/main/java/com/it/cloud/filter/PreRequestLogFilter.java
+++ microservice-gateway-zuul-filter/src/main/java/com/it/cloud/filter/PreRequestLogFilter.java
@@ -9,6 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 
 
 /**
+ * (1) PRE：这种过滤器在请求被路由之前调用。我们可利用这种过滤器实现身份验证、在集群中选择请求的微服务、记录调试信息等。
+ *
+ * (2) ROUTING：这种过滤器将请求路由到微服务。这种过滤器用于构建发送给微服务的请求，并使用Apache HttpClient或Netfilx Ribbon请求微服务。
+ *
+ * (3) POST：这种过滤器在路由到微服务以后执行。这种过滤器可用来为响应添加标准的HTTP Header、收集统计信息和指标、将响应从微服务发送给客户端等。
+ *
+ * (4) ERROR：在其他阶段发生错误时执行该过滤器。
+ *
+ * 除了默认的过滤器类型，Zuul还允许我们创建自定义的过滤器类型。例如，我们可以定制一种STATIC类型的过滤器，直接在Zuul中生成响应，而不将请求转发到后端的微服务。
+ *
+ *
+ *
+ *
+ *
  * filterType：返回过滤器的类型。有pre、route、post、error等几种取值，分别对应上文的几种过滤器。详细可以参考com.netflix.zuul.ZuulFilter.filterType() 中的注释。
  * filterOrder：返回一个int值来指定过滤器的执行顺序，不同的过滤器允许返回相同的数字。
  * shouldFilter：返回一个boolean值来判断该过滤器是否要执行，true表示执行，false表示不执行。
